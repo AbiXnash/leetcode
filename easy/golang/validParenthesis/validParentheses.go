46072cc2c997dbfe99d53572c62000e39ce87959
Abinash <abinash@theabx.in>
2026-03-21 18:36:48 +0530

    new logic with map and n/2 iteration

--- easy/golang/validParenthesis/validParentheses.go
+++ easy/golang/validParenthesis/validParentheses.go
@@ -2,23 +2,34 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
+
+func main() {
+	a := "({})"
+	fmt.Println(a)
+	result := isValid(a)
+	fmt.Println(result)
+}
 
 var parenthesis = map[string]string{
 	"{": "}",
 	"[": "]",
 	"(": ")",
+	"}": "{",
+	"]": "[",
+	")": "(",
 }
 
-func main() {
-	result := IsValid("(}){")
-	fmt.Println(result)
-}
-
-func IsValid(s string) bool {
+func isValid(s string) bool {
 	len := len(s)
+
 	if len%2 != 0 {
 		return false
+	}
+
+	for i := 0; i < len/2; i++ {
+		return strings.Compare(string(s[i]), parenthesis[string(s[len-i-1])]) == 0
 	}
 
 	return true
