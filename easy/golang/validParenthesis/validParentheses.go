53f07dc3e76d8fab4210f225f9f3e740e9893c8a
Abinash <abinash@theabx.in>
2026-03-21 18:06:55 +0530

    removed old logic

--- easy/golang/validParenthesis/validParentheses.go
+++ easy/golang/validParenthesis/validParentheses.go
@@ -1,7 +1,5 @@
 package main
 
-// TODO: this logic is totally wrong i need to implement stack for this
-
 import (
 	"fmt"
 )
@@ -23,19 +21,5 @@ func IsValid(s string) bool {
 		return false
 	}
 
-	for i := 0; i < len; i += 2 {
-		curr := string(s[i])
-		if expected := parenthesis[curr]; expected != getNext(s, i) {
-			return false
-		}
-	}
-
 	return true
-}
-
-func getNext(s string, index int) string {
-	if index+1 < len(s) {
-		return string(s[index+1])
-	}
-	return ""
 }
